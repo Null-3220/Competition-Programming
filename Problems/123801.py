e4caad63a060ff5f8c859617059864483465cdb1
Null <null32209@gmail.com>
2026-09-09 13:13:09 +0330

    Update

---
+++ Problems/123801.py
@@ -0,0 +1,31 @@
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
+
+for i in range(5):
+    # Three middle numbers of the top disk
+    tx = [
+        x[(i - 1) % 5],
+        x[i],
+        x[(i + 1) % 5]
+    ]
+
+    for j in range(5):
+        # Three middle numbers of the bottom disk
+        ty = [
+            y[(j - 1) % 5],
+            y[j],
+            y[(j + 1) % 5]
+        ]
+
+        # Digit-wise sum
+        number = 0
+
+        for k in range(3):
+            digit = (tx[k] + ty[k]) % 10
+            number = number * 10 + digit
+
+        if number % 6 == 0:
+            print("Boro joloo :)")
+            exit()
+
+print("Gir oftadi :(")
